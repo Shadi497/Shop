c0624bf6c7a1fceee5414656a66e8b4a7b7c51c3
Shadi497 <b.shadi.h@gmail.com>
2021-01-20 16:38:01 +0300

    Added Products

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import img from "./pharamcy.jpg";
 import "./App.css";
 import products from "./products";
 
+const style = {
+  shopImage: {
+    display: "block",
+    marginRight: "auto",
+    marginLeft: "auto",
+    width: "100%",
+  },
+};
 function App() {
   const productList = products.map((product) => (
     <td className="cl">
       <tr>{product.image}</tr>
-      <tr className="txt1">{product.name}</tr>
+      <tr className="txt">{product.name}</tr>
       <tr className="prtxt1">{product.price}</tr>
     </td>
   ));
@@ -17,7 +25,7 @@ function App() {
         <h1 className="black">SHADI'S PHARMACY </h1>
         <p className="black">A better way to shop for health & beauty!</p>
 
-        <img className="style" src={img} alt="logo" />
+        <img style={style.shopImage} src={img} alt="logo" />
       </header>
       <body>
         <h1>Products</h1>

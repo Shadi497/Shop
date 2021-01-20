13fe73c5e2a778217ff4490f978b0b9b43091697
Shadi497 <b.shadi.h@gmail.com>
2021-01-20 12:56:10 +0300

    Store Created

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import img from "./pharamcy.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="black">SHADI'S PHARMACY </h1>
+        <p className="black">A better way to shop for health & beauty!</p>
+
+        <img src={img} alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;

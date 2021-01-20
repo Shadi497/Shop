dbba83a67e8aad65c581f8b3951222720509c1c1
Shadi497 <b.shadi.h@gmail.com>
2021-01-20 13:14:44 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <h1 className="black">SHADI'S PHARMACY </h1>
         <p className="black">A better way to shop for health & beauty!</p>
 
-        <img src={img} alt="logo" />
+        <img className="style" src={img} alt="logo" />
       </header>
     </div>
   );
